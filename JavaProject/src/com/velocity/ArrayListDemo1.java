d74d17e6ea6cffe15d8e8230ede121bda44df82e
SHIVAM PANCHAL <shivammpanchal2002@gmail.com>
2024-11-27 16:44:57 +0530

    Elements are added in Arraylist

---
+++ JavaProject/src/com/velocity/ArrayListDemo1.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo1 {
+
+	public static void main(String[] args) {
+		
+		List<Integer> l = new ArrayList<Integer>();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(40);
+		System.out.println(l);
+	}
+}
